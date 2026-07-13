57860b1320e7d58ad591e818646852ae2f66b95c
MarkusHolstein <mh@perspeqtive.de>
2026-07-13 16:56:17 +0200

    Fix logic bug

--- src/Admin/TemplatesAdmin.php
+++ src/Admin/TemplatesAdmin.php
@@ -71,7 +71,7 @@ class TemplatesAdmin extends Admin
         }
 
         if ($disabled === []) {
-            return '1 != 0';
+            return ' || 1 != 0';
         }
 
         return ' || (' . implode(' && ', $disabled) . ')';
